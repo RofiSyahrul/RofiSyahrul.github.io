8f0478dd17f14b9b88c62a0c09d400405280b36d
Rofi Syahrul <srofi44@gmail.com>
2019-10-22 14:18:00 +0700

    Updates

---
+++ precache-manifest.46c2a3627f3c0234fa60ecb792c2ca16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed6b934d5b6af54738b344057596754d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0a48302b7b9af018bf1",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "19d6bc7c34e16fc262b7",
+    "url": "/static/js/2.2345637b.chunk.js"
+  },
+  {
+    "revision": "a0a48302b7b9af018bf1",
+    "url": "/static/js/main.92481ea4.chunk.js"
+  },
+  {
+    "revision": "2d7e2d9d70e6fcdcc7d5",
+    "url": "/static/js/runtime-main.6f76e80d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
